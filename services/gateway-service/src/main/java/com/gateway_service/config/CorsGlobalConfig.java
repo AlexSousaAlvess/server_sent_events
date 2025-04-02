3c3a643d7f9c0d50e8b0006529c91264ba36a2ef
Alex Sousa <alexsousaalves317@gmail.com>
2025-04-01 21:14:48 -0300

    update

--- services/gateway-service/src/main/java/com/gateway_service/config/CorsGlobalConfig.java
+++ services/gateway-service/src/main/java/com/gateway_service/config/CorsGlobalConfig.java
@@ -15,7 +15,7 @@ public class CorsGlobalConfig {
     public CorsWebFilter corsWebFilter() {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowedOrigins(List.of("http://localhost:3000"));
-        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
 
